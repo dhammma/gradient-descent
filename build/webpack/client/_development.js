750ee1a78bffdb9569365e60187fa915bf811c79
Dmitrii Salenko <dmitrii.salenko@litslink.com>
2016-11-28 21:56:23 +0200

    Initial commit (started with redux-starter-kit)

--- build/webpack/client/_development.js
+++
@@ -1,9 +0,0 @@
-export default function (config) {
-  config.devtool = 'eval';
-  config.entry.app.push(
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/dev-server'
-  );
-
-  return config;
-};
--- server/scripts/start.js
+++
@@ -1,5 +0,0 @@
-const server      = require('../index'),
-      SERVER_PORT = require('../../config');
-
-server.listen(SERVER_PORT);
-console.log(`Server listening on port ${SERVER_PORT}.`);
